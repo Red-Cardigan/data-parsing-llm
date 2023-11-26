7d8a6c50431cb44d8748b1a2782fcebb434f4d8c
CardiGPT <jethro@usemozart.info>
2023-11-26 22:36:10 +0000

    Updated prompts as functions

--- esg_llm/schema_processor.py
+++ esg_llm/schema_processor.py
@@ -34,24 +34,109 @@ OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
 # - Needs GPT-4 to get concise strings as responses. Presumably will apply to generative answers when we do these
 # - Applying regex validation rather than a manager prompt works consistently for int and float.
 
-unit_template = Template("Find the most appropriate unit to store the data point described below.\
-                         For example, int, %, year, str etc.\nfield name: $field_name\nfield description: $description\
-                         \n return your answer as 'unit:<>'")
+unit_template = Template("""
+    Determine the most appropriate data type for storing a given data point.
 
-questions_template = Template("Provide questions to help a trainee ESG analyst find information which matches the description in\
-                               a corpus of documents.\nfield name: $field_name\ndescription: $description\nphrase your answer as a\
-                               list of questions'")
+    Parameters:
+    field_name (str): The name of the field.
+    description (str): The description of the data to be stored in the field.
 
-search_template = Template("Use these questions and description to return relevant information from the context below. \
-                           Return only relevant information in bullet points.\nQuestions: $questions\ndescription: $description\
-                           \nContext: $context")
+    Returns:
+    str: A template string with the recommended data type for the field.
+                         
+    Data:
+    field_name: $field_name
+    description: $description
+    """)
+                         
+                        #  Find the most appropriate unit to store the data point described below.\
+                        #  For example, int, %, year, str etc.\nfield name: $field_name\nfield description: $description\
+                        #  \n return your answer as 'unit:<>'")
 
-meta_template = Template("Use the context below to find a $target_unit which matches the description\nfield name: $field_name\
-                         \ndescription: $description\ncontext: $context\nunit: $target_unit\n return your answer as 'final answer:<>'")
+questions_template = Template("""
+    Generate questions to help an ESG analyst find information matching the description in a corpus of documents.
 
-meta_template_string = Template("Use the information provided to return an which matches the description in $target_unit.\n\
-                                field name: $field_name\ndescription: $description\ncontext: $context\nunit: $target_unit\n\
-                                return your answer as 'final answer:<>'")
+    Parameters:
+    field_name (str): The name of the field.
+    description (str): The description of the information to be found.
+
+    Returns:
+    str: A list of questions formatted as a string to guide the ESG analyst.
+                              
+    Data:
+    field_name: $field_name
+    description: $description         
+    """)
+    
+    # "Provide questions to help a trainee ESG analyst find information which matches the description in\
+    #                            a corpus of documents.\nfield name: $field_name\ndescription: $description\nphrase your answer as a\
+    #                            list of questions'")
+
+search_template = Template("""
+    Use the provided questions and description to extract relevant information from a given context.
+
+    Parameters:
+    questions (str): The questions to guide the search.
+    description (str): The description of the information sought.
+    context (str): The context or corpus from which the information is to be extracted.
+
+    Returns:
+    str: A string with relevant information extracted from the context, formatted as bullet points.
+                           
+    Data:
+    questions: $questions
+    description: $description
+    context: $context
+    """)
+    
+    # "Use these questions and description to return relevant information from the context below. \
+    #                        Return only relevant information in bullet points.\nQuestions: $questions\ndescription: $description\
+    #                        \nContext: $context")
+
+meta_template = Template("""
+    Use the provided context to find a value that matches the description in a specified unit.
+
+    Parameters:
+    target_unit (str): The unit in which the matching value is to be found.
+    field_name (str): The name of the field.
+    description (str): The description of the information sought.
+    context (str): The context or corpus from which the information is to be extracted.
+
+    Returns:
+    str: A string representing the final answer in the specified target unit.
+
+    Data:
+    target_unit: $target_unit
+    field_name: $field_name
+    description: $description
+    context: $context
+    """)
+    
+    # "Use the context below to find a $target_unit which matches the description\nfield name: $field_name\
+    #                      \ndescription: $description\ncontext: $context\nunit: $target_unit\n return your answer as 'final answer:<>'")
+
+meta_template_string = Template("""
+    Use the provided context to find a value that matches the description in a specified unit.
+
+    Parameters:
+    target_unit (str): The unit in which the matching value is to be found.
+    field_name (str): The name of the field.
+    description (str): The description of the information sought.
+    context (str): The context or corpus from which the information is to be extracted.
+
+    Returns:
+    str: A string representing the final answer in the specified target unit.
+
+    Data:
+    target_unit: $target_unit
+    field_name: $field_name
+    description: $description
+    context: $context
+    """)
+    
+    # "Use the information provided to return an which matches the description in $target_unit.\n\
+    #                             field name: $field_name\ndescription: $description\ncontext: $context\nunit: $target_unit\n\
+    #                             return your answer as 'final answer:<>'")
 
 
 def create_json_schema(inputs: List[Dict[str, Any]]) -> Dict[str, Any]:
@@ -96,7 +181,7 @@ def create_json_schema(inputs: List[Dict[str, Any]]) -> Dict[str, Any]:
         #Find relevant content in the input based on generated questions
         retrieved_context = search_template.substitute(questions = generated_questions, description = description, context = context)
         #Find a good target unit with GPT-3
-        target_unit = re.findall(r'unit:\s*([^.]+)', phrase_prompt(target_unit_raw, OPENAI_API_KEY, max_tokens=20))
+        target_unit = re.findall(r'unit:\s*([^.\s]+)', phrase_prompt(target_unit_raw, OPENAI_API_KEY, max_tokens=20))
         
         # type_mappings.get(variable_type, {"unit": "", "validation": ""})
 
